d5d3431557005407d69bf6de8f2fac5dc4ee8ae1
Andrew Kawabata <andrewkawabata@Andrews-MacBook-Pro.local>
2021-02-19 11:52:23 -0500

    cleaned up

--- script.jl
+++ script.jl
@@ -3,6 +3,6 @@ using SparseArrays
 function sparsifyGraph(sourceNodes, targetNodes)
     weights = ones(size(sourceNodes))
     G = SparseArrays.sparse(sourceNodes, targetNodes, weights)
-    Gsparse = Laplacians.sparsify(G, ep=3)
+    Gsparse = Laplacians.sparsify(G, ep=2)
     return Gsparse
 end
